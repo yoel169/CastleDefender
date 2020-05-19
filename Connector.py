d3026513a91d74d6d256367fd0634958dfcadcfc
yoel1 <yroba002@fiu.edu>
2020-05-19 19:40:50 -0400

    Added working connect menu

--- Connector.py
+++ Connector.py
@@ -5,14 +5,12 @@ import pickle
 class Connect:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "10.0.0.183"
         self.port = 5555
-        self.addr = (self.server, self.port)
-        self.connect()
 
-    def connect(self):
+    def connect(self, ip):
         try:
-            self.client.connect(self.addr)
+            self.client.connect((ip, self.port))
+            return True
             # return pickle.loads(self.client.recv(2048))
         except:
             return None
